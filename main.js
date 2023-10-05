67a8f0b9bb8355622ffda852b7218fb542f9cfd3
unknown <addie@gmail.com>
2023-10-05 11:17:23 -0700

    Counter

---
+++ main.js
@@ -0,0 +1,23 @@
+let currentInputValue = document.getElementById('todo').innerHTML;
+const addButton = document.getElementById('add-btn');
+const removeButton = document.getElementById('remove-btn');
+const resetButton = document.getElementById('reset-btn');
+
+// Add event listener for "Add" button.
+addButton.addEventListener('click', () => {
+    currentInputValue++
+    document.getElementById('todo').innerHTML = currentInputValue;
+});
+
+// Add event listener for "Remove" button.
+removeButton.addEventListener('click', () => {
+    if (currentInputValue =< 0) return;
+    currentInputValue--
+    document.getElementById('todo').innerHTML = currentInputValue;
+});
+
+// Add event listener for "Reset" button.
+resetButton.addEventListener('click', () => {
+    currentInputValue = 0;
+    document.getElementById('todo').innerHTML = 0;
+});
